650151637845732c9a71f543a7eadc378d0fe426
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2025-01-20 14:29:50 -0300

    feat: add spring project

---
+++ mock-data-lab/src/main/java/br/com/mock_data_lab/MockDataLabApplication.java
@@ -0,0 +1,13 @@
+package br.com.mock_data_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockDataLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockDataLabApplication.class, args);
+	}
+
+}
---
+++ mock-data-lab/src/test/java/br/com/mock_data_lab/MockDataLabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mock_data_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockDataLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
