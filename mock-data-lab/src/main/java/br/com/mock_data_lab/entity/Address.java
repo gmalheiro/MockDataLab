9ba1097c45e0100c33f74f337b13ae44b380373d
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2025-01-20 22:59:35 -0300

    feat: add entity classes for Address, Company, and Person

---
+++ mock-data-lab/src/main/java/br/com/mock_data_lab/entity/Address.java
@@ -0,0 +1,25 @@
+package br.com.mock_data_lab.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String streetAddress;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+}
